94cd8e5a9dc3f666493f17e6a414e4ef89632200
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2020-11-23 15:01:16 +0300

    Add Shopping Basket bar button item with Badge

--- TinyGrocery/UI/Home/HomeController.swift
+++ TinyGrocery/UI/Home/HomeController.swift
@@ -8,22 +8,59 @@
 import UIKit
 
 class HomeController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  
+  // MARK: Properties
+  
+  /// Badge Size for shopiing basket bar buttun item
+  let badgeSize: CGFloat = 20
+  
+  // MARK: Outlets
+  
+  @IBOutlet weak var basketBarButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    showBadge(withCount: 0)
+    navigationItem.title = "Tiny Grocery"
+  }
+  
+  
+  // MARK: Functions
+  
+  func badgeLabel(withCount count: Int) -> UILabel {
+    let badgeCount = UILabel(frame: CGRect(x: 0, y: 0, width: badgeSize, height: badgeSize))
+    
+    badgeCount.translatesAutoresizingMaskIntoConstraints = false
+    badgeCount.layer.cornerRadius = badgeCount.bounds.size.height / 2
+    badgeCount.textAlignment = .center
+    badgeCount.layer.masksToBounds = true
+    badgeCount.textColor = .black
+    badgeCount.font = badgeCount.font.withSize(10)
+    badgeCount.backgroundColor = .white
+    badgeCount.layer.borderWidth = 0.1
+    badgeCount.layer.borderColor = UIColor.lightGray.cgColor
+    badgeCount.text = String(count)
+    
+    return badgeCount
+  }
+  
+  func showBadge(withCount count: Int) {
+    let badge = badgeLabel(withCount: count)
+    basketBarButton.addSubview(badge)
+    // Constraints
+    NSLayoutConstraint.activate([
+      badge.leftAnchor.constraint(equalTo: basketBarButton.leftAnchor, constant: 32),
+      badge.topAnchor.constraint(equalTo: basketBarButton.topAnchor, constant: 4),
+      badge.widthAnchor.constraint(equalToConstant: badgeSize),
+      badge.heightAnchor.constraint(equalToConstant: badgeSize)
+    ])
+  }
+  
+  // MARK: Actions
+  
+  @IBAction func basketButtonPressed(_ sender: UIButton) {
+    print("pressed")
+  }
+  
 }
